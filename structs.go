b618ccc38a580f7d745a6efe095bc37878c27dce
Gino Quispe Calixto <49618571+GinoQuispe21@users.noreply.github.com>
2021-01-08 22:54:15 -0500

    Structs in Golang =D

---
+++ structs.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+type User struct {
+	age             int
+	name, last_name string
+}
+
+func main() {
+	var Gino User
+
+	fmt.Println(Gino)
+
+	Gino.age = 19
+	Gino.name = "Gino"
+	Gino.last_name = "Quispe Calixto"
+
+	fmt.Println(Gino)
+
+	Roro := User{age: 20, name: "Rodrigo", last_name: "Ramirez Bracamonte"}
+
+	fmt.Println(Roro)
+
+	Yuanca := User{18, "Juan Carlos", "Hernandez  Castillo"}
+
+	fmt.Println(Yuanca)
+
+	leyva := new(User) //Declara una estructura que retorna un puntero
+
+	fmt.Println(leyva)
+	fmt.Println(leyva.age, leyva.name, leyva.last_name)
+
+	(*leyva).age = 18
+
+	leyva.name = "Juan Alonso"
+	leyva.last_name = "Leyva Calle"
+
+	fmt.Println(leyva.age, leyva.name, leyva.last_name)
+}
