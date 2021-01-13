803636d2d80cc679b8d61bc62a7628cf1d9d904c
Gino Quispe Calixto <49618571+GinoQuispe21@users.noreply.github.com>
2021-01-13 16:24:48 -0500

    Added defer in read_lines.go :zap:

--- read_lines.go
+++ read_lines.go
@@ -6,8 +6,13 @@ import (
 	"os"
 )
 
-func main() {
+func read_lines_file() bool {
 	file, err := os.Open("./file.txt")
+
+	defer func() {
+		file.Close()
+		fmt.Println("Defer")
+	}() //-> this function executes after the function returns something
 
 	if err != nil {
 		fmt.Printf("There was an error trying to open the file")
@@ -15,9 +20,24 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 
-	for scanner.Scan() {
-		line := scanner.Text()
+	var i int
 
+	for scanner.Scan() {
+		i++
+		line := scanner.Text()
+		fmt.Println(i)
 		fmt.Println(line)
 	}
+
+	if true {
+		return true
+	}
+
+	//file.Close()
+	return true
+}
+
+func main() {
+	execution := read_lines_file()
+	fmt.Println(execution)
 }
